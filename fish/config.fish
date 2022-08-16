9668032ae88787e021e5f2b7b91bb534717bc967
hutchisont <24536068+hutchisont@users.noreply.github.com>
2022-08-16 00:36:02 -0500

    Update fish config so git signed commiting works

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
     set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
     source /usr/share/powerline/bindings/fish/powerline-setup.fish
     powerline-setup
 
     # Scripts of things I've installed manually and don't want to pollute the system files with
-    export PATH="$PATH:/home/tyler/workspace/programs"
+    set -x GPG_TTY $(tty)
+    set -x PATH "$PATH:/home/tyler/workspace/programs"
     
     alias restartPlasma='kquitapp5 plasmashell && kstart5 plasmashell > /dev/null 2>&1'
     alias gits='git status'
