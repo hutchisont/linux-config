7cf66a42aea330b3d991a4864c60002616c8184c
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-09-17 03:31:15 -0500

    nvim: esc gets you out of terminal mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -316,6 +316,8 @@ function do_setup_keymaps()
   vim.keymap.set("n", "n", "nzzzv")
   vim.keymap.set("n", "N", "Nzzzv")
 
+  vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
   vim.keymap.set("x", "<leader>p", "\"_dP")
 
   vim.keymap.set("n", "<leader>y", "\"+y")
