433bac45d713d491baf16b4bf33ed0047913bf51
Tyler Hutchison <tylerhutchison96@gmail.com>
2024-12-16 20:06:16 -0600

    Move which-key to its own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,26 +35,7 @@ require('lazy').setup({
   require 'plugins.oil',
   require 'plugins.telescope',
   require 'plugins.treesitter',
-
-  -- Useful plugin to show you pending keybinds.
-  {
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').add {
-        { '<leader>c', group = '[C]ode' },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      }
-    end,
-  },
+  require 'plugins.which-key',
 
   {
     -- Add indentation guides even on blank lines
