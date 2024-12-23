f9c948dc5c9530fef8e066ac89d7b94674daeb88
Tyler Hutchison <tylerhutchison96@gmail.com>
2024-12-23 10:39:12 -0600

    use custom scripts with nwg-bar to fix restart/shutdown time after using bottles

---
+++ programs/custom_shutdown.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# bottles likes to leave some processes open even after closing it. This results
+# in systemd waiting for two minutes for them to finish up (and if it didn't have
+# a timeout then it would probably just wait forever)
+# So just kill bwrap which gets everything stopped
+
+killall -9 bwrap
+systemctl -i poweroff
