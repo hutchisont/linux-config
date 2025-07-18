bda257e946a0741c78a96dbdb883079f2a78daf7
Tyler Hutchison <tylerhutchison96@gmail.com>
2025-07-18 01:06:08 -0500

    Don't need to specify abs path for ols when using my patched version

--- nvim/lua/personal/enabled_lsps.lua
+++ nvim/lua/personal/enabled_lsps.lua
@@ -1,7 +1,7 @@
 function enable_ols()
-  vim.lsp.config('ols', {
-    cmd = { '/home/tyler/workspace/3rd_party_repos/ols/ols' },
-  })
+  -- vim.lsp.config('ols', {
+  --   cmd = { '/home/tyler/workspace/3rd_party_repos/ols/ols' },
+  -- })
   vim.lsp.enable('ols')
 end
 
