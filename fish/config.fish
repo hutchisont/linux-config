e0c16704f2839afd08c58c94b8763c3a4ba04d5c
Tyler Hutchison <tylerhutchison96@gmail.com>
2026-02-15 04:50:44 -0600

    fish: remove now unused things from fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fzf --fish | source
+    set -x EDITOR nvim
+    set -x BROWSER brave
 
-    set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
-    source /usr/share/powerline/bindings/fish/powerline-setup.fish
-    powerline-setup
+    fzf --fish | source
 
     # Scripts of things I've installed manually and don't want to pollute the system files with
     set -x GPG_TTY $(tty)
@@ -14,11 +13,8 @@ if status is-interactive
     fish_add_path "$HOME/.local/bin"
     #set -x PATH "$HOME/.pyenv/bin:$PATH"
 
-    set -x EDITOR nvim
-
     #pyenv init - | source
 
-    alias vim='nvim'
     alias gits='git status'
     alias gitf='git fetch'
     alias gitfa='git fetch --all --tags --force'
@@ -26,12 +22,4 @@ if status is-interactive
     alias gloga='git log --oneline --decorate --all'
     alias ls='ls -a --color=auto'
     alias lsal='ls -al --color=auto'
-
-    function cdir
-        set last_arg (history | head -n 1 | string split ' ' | tail -n +2 | string join ' ')
-        set dir (dirname $last_arg)
-        set expanded_dir (string replace --regex '^~' $HOME $dir)
-        cd $expanded_dir
-    end
-
 end
